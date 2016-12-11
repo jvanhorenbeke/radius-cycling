7dfd6d0d9b40a0ae4b1fed38e4ffd57c66671d71
Jelle Vanhorenbeke <jelle.jdc@gmail.com>
2016-12-11 02:18:49 -0800

    remove extra slash

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -2,7 +2,7 @@ var athletesMap = new Map();
 var fastestRider;
 var maxSpeed = 0;
 var genesisEpoch = 1481415064;
-var serverUrl = 'https://radiant-taiga-80822.herokuapp.com/';
+var serverUrl = 'https://radiant-taiga-80822.herokuapp.com';
 
 //Compose template string
 String.prototype.compose = (function (){
